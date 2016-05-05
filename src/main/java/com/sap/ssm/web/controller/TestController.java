1ddb3ece241bf1194ec3b808cbbbb60e057f8329
David Lin <david.lin02@sap.com>
2016-05-05 15:34:13 +0800

    change version to 2.5, add test api

---
+++ src/main/java/com/sap/ssm/web/controller/TestController.java
@@ -0,0 +1,14 @@
+package main.java.com.sap.ssm.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() {
+		return "test";
+	}
+}
