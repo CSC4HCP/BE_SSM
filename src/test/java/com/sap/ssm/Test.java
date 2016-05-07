3b7dcc6fbf690f5b2f300580a712c31cd0890c4e
David Lin <linzhicheng1019@live.cn>
2016-05-07 12:12:39 +0800

    change test framework

--- src/test/java/com/sap/ssm/Test.java
+++ src/test/java/com/sap/ssm/Test.java
@@ -6,12 +6,9 @@ package com.sap.ssm;
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -19,13 +16,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@Transactional
-@Rollback(true)
-@ContextHierarchy(@ContextConfiguration(classes = Test.Config.class))
+@ContextConfiguration(classes = Test.Config.class)
 public class Test {
 
 	@Configuration
-	@ComponentScan
+	@ComponentScan(basePackages = "com.sap.ssm.web.testcontroller")
 	@EnableWebMvc
 	public static class Config {
 
