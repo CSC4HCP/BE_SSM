3c104766598dec8882a182ec0b000586c2bfbd63
David Lin <linzhicheng1019@live.cn>
2016-05-07 14:24:56 +0800

    test framework

--- src/test/java/com/sap/ssm/controller/TestControllerTest.java
+++ src/test/java/com/sap/ssm/controller/TestControllerTest.java
@@ -5,11 +5,7 @@ package com.sap.ssm.controller;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultHandler;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,9 +17,6 @@ import com.sap.ssm.WebTest;
 /**
  * @author I326996 David Lin
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = com.sap.ssm.Test.Config.class)
 public class TestControllerTest extends WebTest {
 
 	@Test
