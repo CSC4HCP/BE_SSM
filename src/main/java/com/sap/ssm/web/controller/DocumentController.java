0a201f3792a47f8230843ce317041d219ba125fe
David Lin <david.lin02@sap.com>
2016-05-16 15:23:49 +0800

    add api for query file information and delete file

--- src/main/java/com/sap/ssm/web/controller/DocumentController.java
+++ src/main/java/com/sap/ssm/web/controller/DocumentController.java
@@ -2,7 +2,8 @@ package com.sap.ssm.web.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -16,12 +17,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.chemistry.opencmis.client.api.Document;
 import org.apache.chemistry.opencmis.client.api.Folder;
+import org.apache.chemistry.opencmis.client.api.ItemIterable;
+import org.apache.chemistry.opencmis.client.api.QueryResult;
 import org.apache.chemistry.opencmis.client.api.Session;
 import org.apache.chemistry.opencmis.commons.PropertyIds;
 import org.apache.chemistry.opencmis.commons.data.ContentStream;
 import org.apache.chemistry.opencmis.commons.enums.VersioningState;
 import org.apache.chemistry.opencmis.commons.exceptions.CmisNameConstraintViolationException;
 import org.apache.chemistry.opencmis.commons.exceptions.CmisObjectNotFoundException;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.Transformer;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +40,7 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import com.sap.ecm.api.EcmService;
 import com.sap.ecm.api.RepositoryOptions;
 import com.sap.ecm.api.RepositoryOptions.Visibility;
+import com.sap.ssm.web.model.response.FileDetailResponse;
 
 /**
  * The RestController for Document Service
@@ -89,8 +95,9 @@ public class DocumentController {
 	 * @return the Array of document id or error message if no file uploaded
 	 * @throws IOException
 	 */
-	@RequestMapping(value = "/upload/{session}", method = RequestMethod.POST)
-	public String uploadFiles(HttpServletRequest request, @PathVariable("session") String session) throws IOException {
+	@RequestMapping(value = "/upload/{sessionId}", method = RequestMethod.POST)
+	public String uploadFiles(HttpServletRequest request, @PathVariable("sessionId") String sessionId)
+			throws IOException {
 
 		String message;
 		if (this.multipartResolver.isMultipart(request)) {
@@ -108,15 +115,15 @@ public class DocumentController {
 						Map<String, String> newFileProps = new HashMap<>();
 						newFileProps.put(PropertyIds.OBJECT_TYPE_ID, "cmis:document");
 						newFileProps.put(PropertyIds.NAME, UUID.randomUUID().toString());
-						newFileProps.put(PropertyIds.DESCRIPTION, session);
-						newFileProps.put(PropertyIds.CREATED_BY, request.getRemoteUser());
-						newFileProps.put(PropertyIds.CREATION_DATE, (new Date(System.currentTimeMillis())).toString());
 						ContentStream cStream;
 						try {
 							cStream = openCmisSession.getObjectFactory().createContentStream(
 									mFile.getOriginalFilename(), mFile.getBytes().length, mFile.getContentType(),
 									mFile.getInputStream());
-							rootFolder.createDocument(newFileProps, cStream, VersioningState.NONE);
+							Document document = rootFolder.createDocument(newFileProps, cStream, VersioningState.NONE);
+							Map<String, List<String>> updateProps = new HashMap<>();
+							updateProps.put("sap:tags", Arrays.asList("session" + sessionId));
+							document.updateProperties(updateProps);
 							fileNo++;
 						} catch (CmisNameConstraintViolationException e) {
 							throw new IllegalStateException(e);
@@ -155,5 +162,60 @@ public class DocumentController {
 		response.setHeader("content-disposition",
 				"attachment;filename=" + document.getPropertyValue(PropertyIds.CONTENT_STREAM_FILE_NAME));
 		IOUtils.copy(iStream, response.getOutputStream());
+	}
+
+	/**
+	 * API for query file information<br>
+	 * <br>
+	 * API URL - <b>"/api/document/file/{sessionId}"</b><br>
+	 * Method - <b>"GET"</b>
+	 * 
+	 * @param sessionId
+	 *            session's id
+	 * @return a list of file information
+	 */
+	@RequestMapping(value = "/file/{sessionId}", method = RequestMethod.GET)
+	public Collection<FileDetailResponse> queryFilesBySessionId(@PathVariable("sessionId") String sessionId) {
+		String queryString = "SELECT " + PropertyIds.OBJECT_ID + ", " + PropertyIds.CONTENT_STREAM_FILE_NAME
+				+ " FROM cmis:document WHERE ANY sap:tags IN ('session" + sessionId + "')";
+		if (this.openCmisSession == null) {
+			initOpenCmisSession();
+		}
+		ItemIterable<QueryResult> queryResults = this.openCmisSession.query(queryString, false);
+		return CollectionUtils.collect(queryResults, new Transformer<QueryResult, FileDetailResponse>() {
+
+			@Override
+			public FileDetailResponse transform(QueryResult input) {
+				if (input != null) {
+					return new FileDetailResponse(input);
+				} else {
+					return null;
+				}
+			}
+		});
+	}
+
+	/**
+	 * API for delete a file<br>
+	 * <br>
+	 * API URL - <b>"/api/document/delete/{fileId}"</b><br>
+	 * Method - <b>"DELETE"</b>
+	 * 
+	 * @param fileId
+	 *            file id
+	 * @return state message
+	 */
+	@RequestMapping(value = "/delete/{fileId}", method = RequestMethod.DELETE)
+	public String deleteFile(@PathVariable("fileId") String fileId) {
+		if (this.openCmisSession == null) {
+			initOpenCmisSession();
+		}
+		try {
+			Document document = (Document) this.openCmisSession.getObject(fileId);
+			document.deleteAllVersions();
+			return "File delete successfully!";
+		} catch (CmisObjectNotFoundException e) {
+			return "File not exist!";
+		}
 	}
 }
