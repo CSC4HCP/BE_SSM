4999822ef34eb382f01611f9c7843a191424a02f
David Lin <david.lin02@sap.com>
2016-06-07 10:27:47 +0800

    delete test resources, add webapp structure

---
+++ src/main/webapp/Component.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent", "sap/ui/Device", "ssms/model/Model"
+], function(UIComponent, Device, Model) {
+	"use strict";
+
+	return UIComponent.extend("ssms.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * 
+		 * @public
+		 * @override
+		 */
+		init: function() {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			this.getRouter().initialize();
+			// set the device model
+			this.setModel(Model.createDeviceModel(), "device");
+		}
+	});
+
+});
--- src/main/webapp/controller/App.controller.js
+++ src/main/webapp/controller/App.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("ssms.controller.Home", {
+	return Controller.extend("ssms.controller.App", {
 
 	});
 
