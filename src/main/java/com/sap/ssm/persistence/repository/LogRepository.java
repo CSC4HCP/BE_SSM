b48ea1e49a25f83e43b744d0109e797a9f780d3c
Likai Deng <likai.deng@sap.com>
2016-06-21 15:08:34 +0800

    Fix bugs in findByDateBetween() and deleteLogsSevenDaysAgo() and add JavaDoc

--- src/main/java/com/sap/ssm/persistence/repository/LogRepository.java
+++ src/main/java/com/sap/ssm/persistence/repository/LogRepository.java
@@ -3,7 +3,6 @@ package com.sap.ssm.persistence.repository;
 import java.sql.Timestamp;
 import java.util.List;
 
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +13,26 @@ import com.sap.ssm.persistence.model.Log;
 @Transactional
 public interface LogRepository extends JpaRepository<Log, Long> {
 
+    /**
+     * Find one log by id
+     * @param id log's id
+     * @return List<log> a list of logs
+     */
     List<Log> findByUserId(String userId);
 
-    List<Log> findAll(Specification<Log> sevenDaysAgo);
-
+    /**
+     * Find logs between the given two dates
+     * @param date1 The beginning date
+     * @param date2 The ending date
+     * @return List<log> a list of logs
+     */
     List<Log> findByDateBetween(Timestamp date1, Timestamp date2);
 
-    List<Log> findByDateBefore(Timestamp date1);
+    /**
+     * Find logs before the given date
+     * @param date1
+     * @return List<log> a list of logs
+     */
+    List<Log> findByDateBefore(Long date1);
 
 }
