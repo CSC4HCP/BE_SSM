07f0079b0d85c7cebc40f1251d5128bd5dfc4f5a
Likai Deng <likai.deng@sap.com>
2016-06-21 16:07:40 +0800

    add setId() to Log.java

--- src/main/java/com/sap/ssm/persistence/model/Log.java
+++ src/main/java/com/sap/ssm/persistence/model/Log.java
@@ -3,103 +3,110 @@ package com.sap.ssm.persistence.model;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Entity implementation class for Entity: Log
- * 
  * @author I326996 David Lin
  */
 @Entity
 @Table(name = "T_LOG")
 public class Log implements Serializable {
 
-	/**
-	 * Generated serial version uid
-	 */
-	private static final long serialVersionUID = 8029670391381462673L;
+    /**
+     * Generated serial version uid
+     */
+    private static final long serialVersionUID = 8029670391381462673L;
 
-	@Id
-	@GeneratedValue
-	@Column(name = "ID")
-	private Long id;
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private Long id;
 
-	@Column(name = "USER_ID")
-	private String userId;
+    @Column(name = "USER_ID")
+    private String userId;
 
-	@Column(name = "OPERATION")
-	private String operation;
+    @Column(name = "OPERATION")
+    private String operation;
 
-	@Column(name = "SESSION")
-	private Long session;
+    @Column(name = "SESSION")
+    private Long session;
 
-	@Column(name = "DATE")
-	private Timestamp date;
+    @Column(name = "DATE")
+    private Timestamp date;
 
-	/**
-	 * @return the id
-	 */
-	public Long getId() {
-		return id;
-	}
+    /**
+     * @return the id
+     */
+    public Long getId() {
+	return id;
+    }
 
-	/**
-	 * @return the user
-	 */
-	public String getUserId() {
-		return userId;
-	}
+    /**
+     * Set log Id
+     * @param id log's id
+     */
+    public void setId(Long id) {
+	this.id = id;
+    }
 
-	/**
-	 * @param userId
-	 *            the userId to set
-	 */
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+    /**
+     * @return the user
+     */
+    public String getUserId() {
+	return userId;
+    }
 
-	/**
-	 * @return the operation
-	 */
-	public String getOperation() {
-		return operation;
-	}
+    /**
+     * @param userId the userId to set
+     */
+    public void setUserId(String userId) {
+	this.userId = userId;
+    }
 
-	/**
-	 * @param operation
-	 *            the operation to set
-	 */
-	public void setOperation(String operation) {
-		this.operation = operation;
-	}
+    /**
+     * @return the operation
+     */
+    public String getOperation() {
+	return operation;
+    }
 
-	/**
-	 * @return the session
-	 */
-	public Long getSession() {
-		return session;
-	}
+    /**
+     * @param operation the operation to set
+     */
+    public void setOperation(String operation) {
+	this.operation = operation;
+    }
 
-	/**
-	 * @param session
-	 *            the session to set
-	 */
-	public void setSession(Long session) {
-		this.session = session;
-	}
+    /**
+     * @return the session
+     */
+    public Long getSession() {
+	return session;
+    }
 
-	/**
-	 * @return the date
-	 */
-	public Timestamp getDate() {
-		return date;
-	}
+    /**
+     * @param session the session to set
+     */
+    public void setSession(Long session) {
+	this.session = session;
+    }
 
-	/**
-	 * @param date
-	 *            the date to set
-	 */
-	public void setDate(Timestamp date) {
-		this.date = date;
-	}
+    /**
+     * @return the date
+     */
+    public Timestamp getDate() {
+	return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Timestamp date) {
+	this.date = date;
+    }
 }
